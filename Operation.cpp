8cce33905005907f2ad0e22e16348c7e96ca503a
ParthSingh112 <technoxian543@gmail.com>
2025-04-25 15:29:30 +0530

    Add files via upload
    This s my first operation program

---
+++ Operation.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<math.h>
+#include<string.h>
+#include<stringlist.h>
+#include<search.h>
+#include<Block.h>
+
+using namespace std;
+
+int main()
+{
+
+    string Username,Last_name;
+    long a1,b1,b2,c1,c2,c3;
+
+    cout<<"Username:- ";cin>>Username;
+    cout<<"Last name:- ";cin>>Last_name;
+    cout<<"Enter the password:- ";
+
+cout<<"ALL the rights reserved under the owners and Developers security of program";
+    return 0;
+}
